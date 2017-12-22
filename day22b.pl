15cf4324db4d3a85de1975fdd6202c257e046c9e
Keith Carangelo <keithcarangelo@safetyinsurance.com>
2017-12-22 09:57:06 -0500

    Finished Day22 part b

---
+++ day22b.pl
@@ -0,0 +1,102 @@
+#!/usr/bin/perl
+#
+# $Id: pl.template,v 1.2 2014/07/18 15:01:38 caran Exp $
+#
+use strict;
+use warnings;
+
+use Path::Tiny;
+
+{ package Grid;
+
+  my %new_status = (
+	'c' => 'w',
+	'w' => 'i',
+	'i' => 'f',
+	'f' => 'c',
+  );
+
+  sub move {
+    my ($self) = @_;
+    # Turn right if infected
+    my $pos = "$self->{ pos }[0],$self->{ pos }[1]";
+    my $status = $self->{ grid }{ $pos } || 'c';
+
+    if ($status eq 'c' || $status eq 'i') {
+      my $right = $status eq 'i';
+      if ($self->{ dir }[0] == 1) {
+        $self->{ dir } = $right ? [ 0, 1 ] : [ 0, -1 ];
+       }
+      elsif ($self->{ dir }[0] == -1) {
+        $self->{ dir } = $right ? [ 0, -1 ] : [ 0, 1 ];
+       }
+      elsif ($self->{ dir }[1] == 1) {
+        $self->{ dir } = $right ? [ -1, 0 ] : [ 1, 0 ];
+       }
+      elsif ($self->{ dir }[1] == -1) {
+        $self->{ dir } = $right ? [ 1, 0 ] : [ -1, 0 ];
+       }
+     }
+    elsif ($status eq 'f') {
+      if ($self->{ dir }[0] == 1) {
+        $self->{ dir } = [ -1, 0 ];
+       }
+      elsif ($self->{ dir }[0] == -1) {
+        $self->{ dir } = [ 1, 0 ];
+       }
+      elsif ($self->{ dir }[1] == 1) {
+        $self->{ dir } = [ 0, -1 ];
+       }
+      elsif ($self->{ dir }[1] == -1) {
+        $self->{ dir } = [ 0, 1 ];
+       }
+     }
+
+    $self->{ grid }{ $pos } = $new_status{ $status };
+    $self->{ infected }++ if ($self->{ grid }{ $pos } eq 'i');
+
+    $self->{ pos }[0] += $self->{ dir }[0];
+    $self->{ pos }[1] += $self->{ dir }[1];
+  }
+
+  sub new {
+    my ($class, @input) = @_;
+    my $self = {
+      grid => {},
+	  pos => [0, 0],
+	  dir => [1, 0],	# up
+      infected => 0,
+    };
+    bless $self, $class;
+
+    my $mid = (@input - 1)/2;
+
+    my $row = $mid;
+    for my $line (@input) {
+      my @vals = split( '', $line );
+      for (my $i = 0; $i < @vals; $i++) {
+        if ($vals[$i] eq '#') {
+          my $col = $i - $mid;
+          $self->{ grid }{ "$row,$col" } = 'i';
+         }
+       }
+      $row--;
+     }
+
+    return $self;
+  }
+}
+
+my $input_file = $ARGV[0] || 'input22.txt';
+my $iterations = $ARGV[1] || 5;
+my @input = path( $input_file )->lines_utf8( { chomp => 1 } );
+
+my $grid = Grid->new( @input );
+
+for (my $i = 0; $i < $iterations; $i++) {
+  $grid->move();
+ }
+
+print "The number infected is $grid->{ infected }\n";
+
+exit;
